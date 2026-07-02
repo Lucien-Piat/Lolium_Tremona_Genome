4c575a00be02fcfc07f62448b2aac173f6921bf3
Lucien-Piat <lucienpiat33@gmail.com>
2026-07-02 10:18:24 +0200

    increase time

--- scripts/variant_calling/03_haplotype_caller.sh
+++ scripts/variant_calling/03_haplotype_caller.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
 #SBATCH --mem-per-cpu=2000M        
-#SBATCH --time=04:00:00
+#SBATCH --time=05:00:00
 #SBATCH --output=logs/03_hapcaller_%j.log
 
 set -euo pipefail
