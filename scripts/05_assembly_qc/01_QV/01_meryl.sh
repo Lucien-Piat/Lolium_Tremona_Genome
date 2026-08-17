e007976b11f5aeb8936a09e12ab98ad7e781e607
Lucien-Piat <lucienpiat33@gmail.com>
2026-08-17 15:24:29 +0200

    Fix mem used

--- scripts/05_assembly_qc/01_QV/01_meryl.sh
+++ scripts/05_assembly_qc/01_QV/01_meryl.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 #SBATCH --job-name=meryl
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
-#SBATCH --mem-per-cpu=3G
-#SBATCH --time=4:00:00
+#SBATCH --cpus-per-task=16
+#SBATCH --mem-per-cpu=6G
+#SBATCH --time=06:00:00
 #SBATCH --output=logs/02_meryl_%j.log
 
 # Boilerplate
 set -euo pipefail
-SIF="images/sif/read_profiling.sif"
+SIF="images/sif/genome_analysis.sif"
 READS="./lmultiflorum_tremona_hifi.fastq.gz"
 MERYL_DIR="results/meryl"
-T=${SLURM_CPUS_PER_TASK}
 mkdir -p "${MERYL_DIR}" logs
+
+T=${SLURM_CPUS_PER_TASK}
+MEM=$(( ${SLURM_MEM_PER_CPU%[A-Za-z]*} * T / 1024 )) 
 
 # Build 21mer db
 singularity exec "${SIF}" \
-    meryl count k=21 threads=${T} memory=150 "${READS}" output "${MERYL_DIR}/lmultiflorum.meryl"
+    meryl count k=21 threads=${T} memory=${MEM} "${READS}" output "${MERYL_DIR}/lmultiflorum.meryl"
 
 # Build the histogram
 singularity exec "${SIF}" \
